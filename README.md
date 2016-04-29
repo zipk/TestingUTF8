a1b30e9609f40a3ea01efd7bd44b389e90c22692
BuildTools <s_t@inbox.ru>
2016-04-29 23:51:57 +0200

    Added more comments, more print-outs

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # TestingUTF8
 
+(Main goal of the project - check capabilities/usability of the MS VS2015 integration with github)
+
 Playground to check in C++ various utf8/UNICODE related routines and console apps.
 
+This app tests the following:
+* printing out UNICODE characters to the console via std::wcout.
+* reading out UNICODE characters from the console via std:wcin.
+* storing values in the std::wstring.
+* putting into C++ source code UNICODE const strings (.cpp is with UTF16 LE BOM).
+* all things are checked yet on Windows only (other platforms are in plans).
+
+
 Environment used: MS VS2015 C++
+
--- std_read_write/std_read_write/std_read_write.cpp
+++ std_read_write/std_read_write/std_read_write.cpp
@@ -7,24 +7,53 @@
 #include <io.h>
 #include <fcntl.h>
 
+enum class EInitializeUnicodeInOut {
+	WINDOWS_WAY,
+	STD_WAY
+};
+
 int main()
 {
-	// option A : windows style
-	setlocale(LC_ALL, "");
-	_setmode(_fileno(stdout), _O_WTEXT);
-	_setmode(_fileno(stdin), _O_WTEXT);
 
-	// option B : crossplatform based on STD locales
-	//const std::locale empty_locale = std::locale::empty();
-	//typedef std::codecvt_utf8<wchar_t> converter_type;
-	//const converter_type* converter = new converter_type;
-	//const std::locale utf8_locale = std::locale(empty_locale, converter);
-	//std::wcin.imbue(utf8_locale);
+	// hardcoded way of how initialization shall be done
+	EInitializeUnicodeInOut wayToInit = EInitializeUnicodeInOut::WINDOWS_WAY;
 
+	// init the environment to support UNICODE for in/out
+	switch (wayToInit)
+	{
+	case EInitializeUnicodeInOut::WINDOWS_WAY: {
+		// option A : windows style
+		setlocale(LC_ALL, "");
+		_setmode(_fileno(stdout), _O_WTEXT);
+		_setmode(_fileno(stdin), _O_WTEXT);
+		break;
+	}
+	case EInitializeUnicodeInOut::STD_WAY: {
+		// option B : crossplatform based on STD locales
+		const std::locale empty_locale = std::locale::empty();
+		typedef std::codecvt_utf8<wchar_t> converter_type;
+		const converter_type* converter = new converter_type;
+		const std::locale utf8_locale = std::locale(empty_locale, converter);
+		std::wcin.imbue(utf8_locale);
+	}
+	default:
+		// no initialization?
+		break;
+	}
+
+	// perform some print-outs of test data
+	std::wcout << L"This app tests the following:" << std::endl;
+	std::wcout << L"  - printing out UNICODE characters to the console via std::wcout." << std::endl;
+	std::wcout << L"  - reading out UNICODE characters from the console via std:wcin." << std::endl;
+	std::wcout << L"  - storing values in the std::wstring." << std::endl;
+	std::wcout << L"  - putting into C++ source code UNICODE const strings (.cpp is with UTF16 LE BOM)." << std::endl;
+	std::wcout << L"  - all things are checked yet on Windows only (other platforms are in plans)." << std::endl;
+	std::wcout << std::endl;
 	std::wcout << L"Some texts with various characters: Yes, Süß, Да." << std::endl;
 	std::wcout << std::endl;
 	std::wcout << L"You will be asked for a text, type any to be replied with, or empty to end the application." << std::endl;
 
+	// loop to read a string from std::*in and put it into std::*cout
 	while (true) {
 		std::wstring str;
 		std::wcout << L"Enter/乗り込む/Введите/ያስገቡ/প্রবেশ করান: ";
@@ -35,6 +64,7 @@ int main()
 		std::wcout << L"Введено (ßüs): \"" << str << "\"" << std::endl;
 	}
 
+	// finished
 	std::wcout << L"Финиш, Färtiß, Done." << std::endl;
 
 	return 0;
