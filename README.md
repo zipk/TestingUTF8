c9651fec4f0aef952ed3f38d238d48cbf7ac5290
BuildTools <unconfigured@null.spigotmc.org>
2016-04-29 23:34:16 +0200

    initial version

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TestingUTF8
+
+Playground to check in C++ various utf8/UNICODE related routines and console apps.
+
+Environment used: MS VS2015 C++
---
+++ std_read_write/std_read_write/std_read_write.cpp
@@ -0,0 +1,40 @@
+﻿#include "stdafx.h"
+
+#include <locale>
+#include <codecvt>
+#include <string>
+#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+
+int main()
+{
+	// option A : windows style
+	setlocale(LC_ALL, "");
+	_setmode(_fileno(stdout), _O_WTEXT);
+	_setmode(_fileno(stdin), _O_WTEXT);
+
+	// option B : crossplatform based on STD locales
+	//const std::locale empty_locale = std::locale::empty();
+	//typedef std::codecvt_utf8<wchar_t> converter_type;
+	//const converter_type* converter = new converter_type;
+	//const std::locale utf8_locale = std::locale(empty_locale, converter);
+	//std::wcin.imbue(utf8_locale);
+
+	std::wcout << L"Yes, Süß, Да." << std::endl;
+
+	while (true) {
+		std::wstring str;
+		std::wcout << L"Enter/乗り込む/Введите/ያስገቡ/প্রবেশ করান: ";
+		std::getline(std::wcin, str);
+		if (str.empty()) {
+			break;
+		}
+		std::wcout << L"Введено (ßüs): \"" << str << "\"" << std::endl;
+	}
+
+	std::wcout << L"Финиш, Färtiß, Done." << std::endl;
+
+	return 0;
+}
+
